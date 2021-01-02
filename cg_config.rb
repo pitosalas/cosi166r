cde2b6f6194c56264ac068408b886a1f8cce9aeb
Pito Salas <pitosalas@gmail.com>
2021-01-02 18:11:59 -0500

    checkpoint

--- cg_config.rb
+++ cg_config.rb
@@ -1,47 +1,119 @@
-require "coursegen"
+# frozen_string_literal: true
 
-# bucket for AWS Deployment of the course
-AWS_BUCKET = "cosi166b"
+# Coursegen configuration file.
+#
+# Note that in order to read the configuration file, this file must be at the
+# root of the course directory.
+require 'coursegen'
 
-# Local directory path for directory of content
-CONTENT_PATH = "/mydev/cosi166b"
+# AUTHOR
+AUTHOR_NAME = 'R. Pito Salas'
+AUTHOR_EMAIL = 'pitosalas@brandeis.edu'
+COPYRIGHT_STRING = "Copyright (2013-#{Time.now.year}) #{AUTHOR_NAME}, #{AUTHOR_EMAIL}"
 
-# Copyright string
-COPYRIGHT_STRING = "Copyright (2014-2020) R. Pito Salas, pitosalas@brandeis.edu"
+# Bucket Deployment of the course
+AWS_BUCKET = "cosi166"
 
-# Course short name
-COURSE_SHORT_NAME = "COSI 166b"
-COURSE_LONG_NAME = "Capstone Project for Software Engineering"
-COURSE_ABBREV = "166b"
+# COURSE
+COURSE_SHORT_NAME = 'Cosi166'
+COURSE_LONG_NAME = 'Capstone in Software Engineering'
+COURSE_ABBREV = 'SE'
 
-# Schedule information. Note that Monday is day 0
+# Lectures_schedule defines the schedule of lectures.
+#
+# This is primarily used to 1) figure out the time and date of each class,
+# and 2) figure out the ordering of classes on the sidebar.
+
 LECTURES_SCHEDULE_CONFIG = ScheduleDef.new(
-  first_day: "jan-14-2020",
+  first_day: "feb-2-2021",
   weekdays: [:tuesday, :thursday],
-  start_times: ["15:30", "15:30"],
-  end_times: ["16:50", "16:50"],
-  start_time: "15:30",
-  end_time: "16:50",
-  number: 41,
-  skips: ['feb-18-2020', 'feb-20-2020', 'mar-24-2020', 'apr-9-2020', 'apr-16-2020']
+  start_times: ["12:00", "12:00"],
+  end_times: ["13:30", "13:30"],
+  number: 28,
+  skips: [],
+  start_time: "12:00",
+  end_time: "13:30"
 )
 
-# Sections in the right hand margin of the page
-# Bullet choices are :dash, :star, :plus, :minus
+# Section_config defines the structure of the course.
+#
+# Each section is defined by
+#   title     name of the section
+#   selector  directory of the section (under /content)
+#   type      type of the section (:section or :lecture)
+#   schedule  schedule of the lecture (only needed for :lecture)
+#
+# Additionally, you can choose to hide the section on the sidebar by setting
+# hidden: true.
 SECTION_CONFIG = [
-  SectionDef.new("Introduction", "intro", hidden: false, type: :section, bullet: :star),
-  SectionDef.new("Lectures", "lectures", type: :lecture, schedule: LECTURES_SCHEDULE_CONFIG, bullet: :dash),
-  SectionDef.new("Background", "background", hidden: false, type: :section),
+  SectionDef.new("Intro", "intro", type: :section, collapsed: false, bullet: :star),
+  SectionDef.new("Lectures", "lectures",
+                 type: :lecture, schedule: LECTURES_SCHEDULE_CONFIG, bullet: :dash),
+  SectionDef.new("Homework", "homework", hidden: true, type: :section),
+  SectionDef.new("Course Resources", "background", type: :section, collapsed: false, bullet: :star),
   SectionDef.new("Topics", "topics", hidden: true, type: :section),
-  SectionDef.new("Extras", "extras", hidden: true, type: :section),
-]
+  SectionDef.new("Extras", "extras", hidden: true, type: :section)
+].freeze
 
-# Options are:
-# bullet_style: css style for each bullet
-STYLING_CONFIG = {bullet_style: "\"font-size: 60%; width: 10px; color: grey\""}
+# SITE
+#
+# The URL of the deployed site.
+# Please include any http or https prefix.
+SITE_URL = "http://cosi102r.s3-website-us-east-1.amazonaws.com"
+
+# Home_page sets the home page of the site.
+# This is usually /content/index.md.erb.
+#
+# Note: the .erb suffix is required when setting the HOME_PAGE path.
 HOME_PAGE = "/content/intro/00_syllabus.md.erb"
+
+# Menulinks defines the links on the navigation bar.
+# They are formatted as "Name" - "URL" pair.
+#
+# Examples:
+#   %w[HOME /content/index.md]
+#   %w[TOC /content/course_toc.md]
+MENULINKS = [%w[lectures /content/intro/10_coursetoc.md/],
+            %w[homeworks /content/intro/50_weeks.md/],
+          %w[latte https://moodle2.brandeis.edu/course/view.php?id=21991]].freeze
+
+# Helpful_box provides a feedback box on the sidebar.
+#
+# DO NOT USE. This is currently not functional.
 HELPFUL_BOX = false
-MENULINKS = [["TOC", "/content/intro/01_coursetoc.md/"],
-             ["Project Outline", "/content/intro/20_project_roadmap.md/"],
-             ["Deliverables", "/content/intro/03_term_project.md/"],
-             ["Examples", "http://pitosalas.github.io/brandeisprojects/"]].freeze
+
+# STYLING
+#
+# Options:
+#   bullet_style: css style for each bullet
+STYLING_CONFIG = {
+  bullet_style: '"font-size: 60%; width: 10px; color: grey"'
+}.freeze
+
+# SEARCH
+#
+# Coursegen uses DocSearch by Algolia (https://docsearch.algolia.com/) to
+# provide site-wide full-text search functionality.
+#
+# In order for search to function, you need to replace with your own
+# search API key, and name of your index from Algolia.
+# For details, please refer to https://github.com/pitosalas/coursegen/blob/master/README.md.
+SEARCH_CONFIG = {
+  api_key: '855a55e631c6523e8201162e6d9c1e9c',
+  index_name: 'cosi102r',
+  # APP_ID is only used if you are running DocSearch on your own.
+  app_id: '',
+  debug: false
+}.freeze
+
+# SLIDES
+SLIDES_CONFIG = {
+  # Ignore_selectors ignore certain html elements from rendering in the slides.
+  ignore_selectors: ['li ul'],
+
+  # Reveal.js specific options
+  # See: https://revealjs.com/config/
+  revealjs_opts: {
+    transition: 'slide'
+  }
+}.freeze
